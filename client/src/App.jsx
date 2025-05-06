709539ba8ca5e5db6320a68016603bb4830d02a5
Priya-Ramdawor <priya.ramdawor@spoonconsulting.com>
2025-05-06 22:21:57 +0400

    added navbar and hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-
+import Navbar from "./components/Navbar"
+import {useLocation, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () =>{
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
   return (
     <div>
-
+      {!isOwnerPath && <Navbar/> }
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
