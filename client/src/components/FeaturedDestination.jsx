3b922d2d53a5724164b036530304cef460a6dd14
Priya-Ramdawor <priya.ramdawor@spoonconsulting.com>
2025-05-08 18:10:45 +0400

    added featured section

---
+++ client/src/components/FeaturedDestination.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom'
+import {roomsDummyData} from '../assets/assets'
+import HotelCard from './HotelCard'
+import Title from './Title'
+
+const FeaturedDestination = () => {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+
+      <Title title='Featured Destination' subtitle='Discover our handpicked
+      selection of exceptional hotels, offering unparalleled luxury and unforgettable experience'/>
+
+      <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+        {roomsDummyData.slice(0,3).map((room,index) => (
+          <HotelCard key={room._id} room={room} index={index}/>
+        ))}
+      </div>
+
+      <button onClick={() => {navigate('/rooms'); scrollTo(0,0)}}
+      className='my-16 px-4 py-2 text-sm font-medium border border-gray-500
+      rounded-2xl bg-white hover:bg-gray-100 transition-all cursor-pointer'>
+        View all destinations
+      </button>      
+    </div>
+  )
+}
+
+export default FeaturedDestination
