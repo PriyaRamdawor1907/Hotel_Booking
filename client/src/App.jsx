8b79062a06e5ccab62920f488b47994fd250e991
Priya-Ramdawor <priya.ramdawor@spoonconsulting.com>
2025-05-09 18:15:24 +0400

    complted room details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {useLocation, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from "./components/Footer"
 import AllRooms from "./pages/AllRooms"
+import RoomDetails from "./pages/RoomDetails"
 
 const App = () =>{
 
@@ -15,6 +16,7 @@ const App = () =>{
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path="/rooms" element={<AllRooms/>}/>
+          <Route path="/rooms/:id" element={<RoomDetails/>}/>
         </Routes>
       </div>
       <Footer/>
